117f4b4c26f032dd02a1019709f1fb6fdbe23aa9
reona5 <shimada.reona@gmail.com>
2020-08-09 23:40:10 +0900

    Implement getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,52 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import styled from 'styled-components'
+import { getSortedPostsData } from '../lib/posts'
 
 const Heading = styled.section`
-  font-size: 1.2rem;
-  line-height: 1.5;
+  font-size: 1.5rem;
+  line-height: 1.4;
+  margin: 1rem 0;
 `
 
-const Main = () => {
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`
+
+const ListItem = styled.li`
+  margin: 0 0 1.25rem;
+`
+
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+const Main = ({ allPostsData }) => {
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Heading>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </Heading>
+      <Heading>Blog</Heading>
+      <List>
+        {allPostsData.map(({ id, date, title }) => (
+          <ListItem key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </ListItem>
+        ))}
+      </List>
     </Layout>
   )
 }
